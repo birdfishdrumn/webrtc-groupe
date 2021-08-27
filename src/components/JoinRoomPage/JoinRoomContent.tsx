c7479c26f795dd9aece4df7e32a2fd318d9650ad
michihiro <pomeofishbird0303@gmail.com>
2021-08-28 08:18:55 +0900

    お部屋に接続時の関数、またreduxの状態管理に名前とroomIdの項目を追加

--- src/components/JoinRoomPage/JoinRoomContent.tsx
+++ src/components/JoinRoomPage/JoinRoomContent.tsx
@@ -3,6 +3,10 @@ import ErrorMessage from './ErrorMessage'
 import JoinRoomButtons from './JoinRoomButtons'
 import JoinRoomInput from './JoinRoomInput'
 import OnlyWithAudioCheckbox from './OnlyWithAudioCheckbox'
+import { getRoomExists } from "src/utils/api";
+import { useHistory } from "react-router-dom";
+import { useDispatch } from "react-redux"
+import { setRoomId,setIdentity } from "src/features/room/roomSlice";
 
 interface Props {
   isRoomHost:boolean
@@ -11,10 +15,40 @@ interface Props {
 const JoinRoomContent:React.VFC<Props> = ({isRoomHost}) => {
   const [roomIdValue, setRoomIdValue] = useState("")
   const [nameValue, setNameValue] = useState("")
-  const [errorMessage, setErrorMessage] = useState(null)
+  const [errorMessage, setErrorMessage] = useState<string | null>(null)
 
-  const handleJoinRoom = () => {
+  const dispatch = useDispatch()
+  const history = useHistory()
 
+  const handleJoinRoom = async () => {
+    dispatch(setIdentity(nameValue))
+    if (isRoomHost) {
+     createRoom()
+    } else {
+      await joinRoom()
+   }
+  }
+
+  const joinRoom = async() => {
+     const responseMessage = await getRoomExists(roomIdValue)
+
+    const { roomExists, full } = responseMessage
+    console.log("jj")
+
+    if (roomExists) {
+      if (full) {
+              setErrorMessage("満員です。時間が経ってから再度ご訪問ください。")
+      } else {
+        dispatch(setRoomId(roomIdValue))
+          history.push("/room")
+      }
+    } else {
+      setErrorMessage("お部屋が見つかりません。")
+    }
+  }
+
+  const createRoom = () => {
+     history.push("/room")
   }
   return (
     <>
--- src/features/room/roomSlice.ts
+++ src/features/room/roomSlice.ts
@@ -5,13 +5,15 @@ import { RootState, AppThunk } from '../../app/store';
 export interface RoomState {
   identity: string
   isRoomHost: boolean
-  connectionOnlyWithAudio:boolean
+  connectionOnlyWithAudio: boolean
+  roomId:string | null
 }
 
 const initialState: RoomState = {
   identity: "",
   isRoomHost: false,
-  connectionOnlyWithAudio:false
+  connectionOnlyWithAudio: false,
+  roomId:null
 };
 
 
@@ -27,18 +29,26 @@ export const roomSlice = createSlice({
     },
     setConnectOnlyWithAudio: (state, action) => {
             state.connectionOnlyWithAudio = action.payload;
+    },
+      setRoomId: (state, action) => {
+            state.roomId = action.payload;
+    },
+       setIdentity: (state, action) => {
+            state.identity = action.payload;
     }
   }
 
 });
 
-export const { setRoomHost,setConnectOnlyWithAudio } = roomSlice.actions;
+export const { setRoomHost,setConnectOnlyWithAudio,setRoomId,setIdentity } = roomSlice.actions;
 
 // The function below is called a selector and allows us to select a value from
 // the state. Selectors can also be defined inline where they're used instead of
 // in the slice file. For example: `useSelector((state: RootState) => state.room.value)`
 export const getRoomState = (state: RootState) => state.room.isRoomHost;
 export const getConnectOnlyWithAudio = (state: RootState) => state.room.connectionOnlyWithAudio;
+export const getRoomId = (state: RootState) => state.room.roomId;
+export const getIdentity = (state: RootState) => state.room.identity;
 
 // We can also write thunks by hand, which may contain both sync and async logic.
 // Here's an example of conditionally dispatching actions based on current state.
