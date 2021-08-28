538f0c486b983fed676248488b2441abe23ddc7a
michihiro <pomeofishbird0303@gmail.com>
2021-08-28 19:07:55 +0900

    cameraボタン,退出ボタン、カメラ切り替えボタンの作成

--- src/components/RoomPage/VideoSection/CameraButton.tsx
+++ src/components/RoomPage/VideoSection/CameraButton.tsx
@@ -1,8 +1,19 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import CameraButtonImg from "src/resources/images/camera.svg";
+import CameraButtonImgOff from "src/resources/images/cameraOff.svg";
 const CameraButton = () => {
+  const [isLocalVideoDisabled, setIsLocalVideoDisabled] = useState(false)
+
+  const handleCameraButtonPressed = () => {
+    setIsLocalVideoDisabled(!isLocalVideoDisabled)
+  }
   return (
-    <div>
+    <div className="video_button_container">
+      <img
+        src={isLocalVideoDisabled ? CameraButtonImgOff : CameraButtonImg}
+        className="video_button_image"
+        onClick={handleCameraButtonPressed}
+      />
 
     </div>
   )
--- src/components/RoomPage/VideoSection/SwitchToScreenSharingButton.tsx
+++ src/components/RoomPage/VideoSection/SwitchToScreenSharingButton.tsx
@@ -1,8 +1,19 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import SwitchImg from "src/resources/images/switchToScreenSharing.svg";
 const SwitchToScreenSharingButton = () => {
+  const [isScreenSharingActive, setIsScreenSharingActive] = useState(false)
+
+  const handleScreenShareToggle = () => {
+    setIsScreenSharingActive(!isScreenSharingActive)
+  }
   return (
-    <div>
+
+    <div className="video_button_container">
+      <img
+        src={SwitchImg}
+        onClick={handleScreenShareToggle}
+        className="video_button_image"
+      />
 
     </div>
   )
